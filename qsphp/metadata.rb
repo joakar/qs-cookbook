3d4b611c1178a3fbd6686c8adb1b46f2eda99715
Joakim Karlsson <jocke@covedesign.se>
2015-09-21 16:19:31 +0200

    Added qsphp

---
+++ qsphp/metadata.rb
@@ -0,0 +1,7 @@
+name        "qsphp"
+description "Config PHP"
+maintainer  "AM System"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "deploy"
---
+++ qsphp/specs/configure_spec.rb
@@ -0,0 +1,14 @@
+require 'minitest/spec'
+
+describe_recipe 'php::configure' do
+  include MiniTest::Chef::Resources
+  include MiniTest::Chef::Assertions
+
+  it 'creates the php file for data exchange' do
+    node[:deploy].each do |application, deploy|
+      if deploy[:application_type] == 'php'
+        file("/etc/php.d/qs.ini").must_exist.with(:mode, '0644').and(:owner, 'root').and(:group, 'root')
+      end
+    end
+  end
+end
