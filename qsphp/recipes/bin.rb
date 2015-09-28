60831f5f39c16947ea2cfae664e6c4a90fb1053c
Joakim Karlsson <jocke@covedesign.se>
2015-09-28 16:01:06 +0200

    config database

--- qsphp/recipes/bin.rb
+++ qsphp/recipes/bin.rb
@@ -3,14 +3,6 @@ node[:deploy].each do |application, deploy|
     Chef::Log.debug("Skipping qsphp::configure application #{application} as it is not an PHP app")
     next
   end
-
-  template "/etc/php5/apache2/conf.d/qs.ini" do
-    cookbook 'qsphp'
-    source 'qs.ini.erb'
-    mode '0644'
-    owner 'root'
-    group 'root'
-  end
  
   remote_directory '/var/inc' do
     source 'bin'
